b933fd2a3499abf56bf6af4d01863c66ea220ad8
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-20 13:48:04 -0600

    added some variables in script that we may use

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
 
 //they will select number of articles
 
+//we have variables that must be used during the search click event.
+var apiKey = "yta9zAof52rfg75gILmNs8qy7HgHLa8s";
+
+//these variables change according to user input.
+var searchTerm = "";
+var recordNumberEl = 0;
 //not required, if they have years, we also have to include year range in the search
-
-
+var startYearInputEl = 0;
+var endYearInputEl = 0;
 
 
 //on click submit-- the page queries the NYT API
