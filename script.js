c01d86f36f28f0237e60a0ef2635ecb7d0f6351d
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-20 13:34:23 -0600

    added pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+//When user opens page, they will add input to the search options
+
+//they will add input to .searchInput
+
+//they will select number of articles
+
+//not required, if they have years, we also have to include year range in the search
+
+//on click submit-- the page queries the NYT API
+
+//on click clear results, the page clears their input
